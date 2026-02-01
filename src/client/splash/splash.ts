b4ba9f6800ed2ca1586f440674c6aca321c506c5
Shahen <shahenalgoo@gmail.com>
2026-02-01 19:52:42 +0400

    building splash

--- src/client/splash/splash.ts
+++ src/client/splash/splash.ts
@@ -1,17 +1,17 @@
 import { navigateTo, context, requestExpandedMode } from "@devvit/web/client";
 
 const startButton = document.getElementById(
-  "start-button"
+	"start-button"
 ) as HTMLButtonElement;
 
 startButton.addEventListener("click", (e) => {
-  requestExpandedMode(e, "game");
+	requestExpandedMode(e, "game");
 });
 
-const titleElement = document.getElementById("title") as HTMLHeadingElement;
+const greetUserElement = document.getElementById("greetUser") as HTMLHeadingElement;
 
 function init() {
-  titleElement.textContent = `Hey ${context.username ?? "user"} 👋`;
+	greetUserElement.textContent = `Hey ${context.username ?? "Player"}!`;
 }
 
 init();
